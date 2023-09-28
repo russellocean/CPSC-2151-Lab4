963b9f696face39c4e7fa73295a5bfe783490c86
Russell Welch <russellwelch17@gmail.com>
2023-09-28 15:37:14 -0400

    Implement contracts for ListDoubleQueue

--- src/com/gradescope/DoubleQueue/code/ListDoubleQueue.java
+++ src/com/gradescope/DoubleQueue/code/ListDoubleQueue.java
@@ -2,75 +2,73 @@ package com.gradescope.DoubleQueue.code;
 
 import java.util.ArrayList;
 
-/**ListDoubleQueueContract
+/**
+ * ListDoubleQueueContract
  *
+ * ArrayList implementation for the Double queue.
  *
- * @invariant:
+ * @invariant LQueue != null
  *
- * @corresponds:
+ * @corresponds to LQueue as the underlying data structure for the Double queue.
  *
  */
-public class ListDoubleQueue implements IDoubleQueue
-{
+public class ListDoubleQueue implements IDoubleQueue {
     private ArrayList<Double> LQueue;
     private int maxListSize;
 
-    /**ListDoubleQueueConstructorContact
+    /**
+     * ListDoubleQueueConstructorContract
      *
+     * Constructor for the ListDouble queue.
      *
-     * @param maxSize
+     * @param maxSize Maximum size of the ListDouble queue.
      *
-     * @pre
-     *
-     * @post
+     * @pre maxSize > 0
+     * @post LQueue is initialized AND maxListSize = maxSize
      *
      */
-    public ListDoubleQueue(int maxSize)
-    {
+    public ListDoubleQueue(int maxSize) {
         this.LQueue = new ArrayList<Double>();
         this.maxListSize = maxSize;
     }
 
-    /**enqueueContact
+    /**
+     * enqueueContract
      *
+     * Enqueue adds a value to the queue. If the queue is full, it replaces the last
+     * value.
      *
-     * @param val
+     * @param val The Double to be added.
      *
-     * @pre
-     *
-     * @post
+     * @pre val != null
+     * @post If |LQueue| < maxListSize, val is added to the end of LQueue.
+     *       Otherwise, the last value in LQueue is replaced with val.
      *
      */
     @Override
-    public void enqueue(Double val)
-    {
-        if(LQueue.size() == this.maxListSize)
-            LQueue.set(this.maxListSize-1, val);
+    public void enqueue(Double val) {
+        if (LQueue.size() == this.maxListSize)
+            LQueue.set(this.maxListSize - 1, val);
         else
             LQueue.add(val);
     }
 
-    //Note: The below 3 functions intentionally do not have contracts. You do not need to add them.
+    // Note: The below 3 functions intentionally do not have contracts. You do not
+    // need to add them.
 
     @Override
-    public Double dequeue()
-    {
+    public Double dequeue() {
         return LQueue.remove(0);
     }
 
-
     @Override
-    public int length()
-    {
+    public int length() {
         return LQueue.size();
     }
 
-
-    public String toString()
-    {
+    public String toString() {
         String ret = "";
-        for(Double d : LQueue)
-        {
+        for (Double d : LQueue) {
             ret += ("[" + d + "] ");
         }
         return ret;
